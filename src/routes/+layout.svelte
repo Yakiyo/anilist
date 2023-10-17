e81a20b5b4f67891f47012bdeb5beaeea8be186b
Yakiyo <yakiyo1206@gmail.com>
2023-10-17 10:46:48 +0000

    smoll stupid navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import NavBar from '../components/NavBar.svelte';
+	const items = [
+		{
+			name: 'site',
+			ref: '/'
+		},
+		{
+			name: 'github',
+			ref: 'https://github.com/Yakiyo'
+		}
+	];
+</script>
+
+<NavBar {items}/>
+
+<slot />
