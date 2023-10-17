7f7c628d9ef2a08bab2710e034212136ee394399
Yakiyo <yakiyo1206@gmail.com>
2023-10-17 10:47:05 +0000

    chore: prettier format

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,36 +1,35 @@
 <script>
-    /**
-     * @type Record<string, string>[]
-     */
-    export let items;
+	/**
+	 * @type Record<string, string>[]
+	 */
+	export let items;
 </script>
 
-<style>
-    ul {
-        margin: 0;
-        padding: 0;
-        list-style-type: none;
-        overflow: hidden;
-    }
-
-    li {
-        float: left;
-    }
-
-    li a {
-        display: block;
-        padding: 8px;
-        background-color: rgb(44, 46, 46);
-        color: rgb(243, 243, 243);
-    }
-    li a:hover {
-        background-color: black;
-    }
-
-</style>
-
 <ul>
-    {#each items as item}
-        <li><a href={item.ref}>{item.name}</a></li>
-    {/each}
+	{#each items as item}
+		<li><a href={item.ref}>{item.name}</a></li>
+	{/each}
 </ul>
+
+<style>
+	ul {
+		margin: 0;
+		padding: 0;
+		list-style-type: none;
+		overflow: hidden;
+	}
+
+	li {
+		float: left;
+	}
+
+	li a {
+		display: block;
+		padding: 8px;
+		background-color: rgb(44, 46, 46);
+		color: rgb(243, 243, 243);
+	}
+	li a:hover {
+		background-color: black;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,6 @@
 	];
 </script>
 
-<NavBar {items}/>
+<NavBar {items} />
 
 <slot />
