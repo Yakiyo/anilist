74e71e389d05577860f804dd3288e538041ef1a3
Yakiyo <yakiyo1206@gmail.com>
2023-11-08 16:12:05 +0000

    feat: add link items

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,31 @@
 <script>
 	const themes = ['Default', 'Dark', 'High Contrast', 'System Theme'];
+	const links = [
+		[
+			{ name: 'Logout', url: '#' },
+			{ name: 'Donate', url: '/donate' },
+			{ name: 'AniList.co', url: 'https://anilist.co' },
+			{ name: 'AniChar', url: 'https://anichart.net/' }
+		],
+		[
+			{ name: 'Apps', url: '/apps' },
+			{ name: 'Site Stats', url: '/site-sites' },
+			{ name: 'Recommendations', url: '/recommendations' },
+			{ name: 'API', url: 'https://github.com/AniList/ApiV2-GraphQL-Docs' }
+		],
+		[
+			{ name: 'Discord', url: 'https://discord.gg/TF428cr' },
+			{ name: 'Twitter', url: 'https://twitter.com/AniListco' },
+			{ name: 'Facebook', url: 'https://www.facebook.com/AniListco' },
+			{ name: 'Github', url: 'https://github.com/AniList' }
+		],
+		[
+			{ name: 'Add Data', url: 'https://submission-manual.anilist.co/' },
+			{ name: 'Moderators', url: 'https://anilist.co/moderators' },
+			{ name: 'Contact', url: 'mailto:contact@anilist.co' },
+			{ name: 'Site Map.xml', url: 'https://anilist.co/sitemap/index.xml' }
+		]
+	];
 </script>
 
 <footer>
@@ -11,9 +37,15 @@
 			{/each}
 		</ul>
 	</div>
-	<div class="links" />
-	<div class="links" />
-	<div class="links" />
+	{#each links as link}
+		<div class="links">
+			<ul>
+				{#each link as item}
+					<li><a href={item.url}>{item.name}</a></li>
+				{/each}
+			</ul>
+		</div>
+	{/each}
 </footer>
 
 <style>
@@ -33,7 +65,11 @@
 		padding: 30px;
 	}
 
-	.themes ul {
+    footer a {
+        text-decoration: none;
+    }
+
+	ul {
 		list-style-type: none;
 		padding: 0;
 	}
