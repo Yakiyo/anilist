759b197eda9896fe2c2893563199bc1c2ff3d38f
Yakiyo <yakiyo1206@gmail.com>
2023-11-08 15:08:29 +0000

    chore: more layout stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,25 @@
+<script>
+	const user = 'Yakiyo';
+	const menuItems = [
+		{ name: 'Home', path: '/' },
+		{ name: 'Profile', path: `/user/${user}` },
+		{ name: 'Anime List', path: `/user/${user}/animelist` },
+		{ name: 'Manga List', path: `/user/${user}/mangalist` },
+		{ name: 'Browse', path: '/search/anime' },
+		{ name: 'Forum', path: '/forum/overview' }
+	];
+</script>
+
 <nav>
-	<a href="/"><img src="https://anilist.co/img/icons/icon.svg" alt="anilist-logo" /></a>
-	<span>Menus</span>
-	<span>Search and User</span>
+	<div class="nav-item">
+		<a href="/"><img src="https://anilist.co/img/icons/icon.svg" alt="anilist-logo" /></a>
+	</div>
+	<div class="nav-item menu">
+		{#each menuItems as item}
+			<a href={item.path} class={item.name === "Browse" ? "browse" : ""}>{item.name}</a>
+		{/each}
+	</div>
+	<div class="nav-item">Search and User</div>
 </nav>
 <slot />
 
@@ -17,10 +35,34 @@
 		padding: 0;
 		margin: 0;
 		width: 100%;
+		font-size: 14px;
 	}
 	nav img {
 		height: 50px;
 	}
+
+	.nav-item {
+		flex-grow: 1;
+		text-align: center;
+	}
+
+	.menu {
+		flex-grow: 3;
+	}
+
+	.menu {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		max-width: 60%;
+	}
+
+	.menu > a {
+		padding: 2%;
+		text-decoration: none;
+		color: #a0b1c5;
+	}
+
 	:global(body) {
 		background-color: #0b1622;
 	}
