429133bf54a12ca2182b1c42690477532df9d140
Yakiyo <yakiyo1206@gmail.com>
2023-11-08 14:29:16 +0000

    chore: smoll edits

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,27 @@
-<script lang="ts">
-	import NavBar from '../components/NavBar.svelte';
-	const items = [
-		{
-			name: 'site',
-			ref: '/'
-		},
-		{
-			name: 'github',
-			ref: 'https://github.com/Yakiyo'
-		}
-	];
-</script>
-
-<NavBar {items} />
-
+<nav>
+	<a href="/"><img src="https://anilist.co/img/icons/icon.svg" alt="anilist-logo" /></a>
+	<span>Menus</span>
+	<span>Search and User</span>
+</nav>
 <slot />
+
+<style>
+	nav {
+		background-color: #152232;
+		color: #a0b1c5;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		height: 60px;
+		top: 0;
+		padding: 0;
+		margin: 0;
+		width: 100%;
+	}
+	nav img {
+		height: 50px;
+	}
+	:global(body) {
+		background-color: #0b1622;
+	}
+</style>
