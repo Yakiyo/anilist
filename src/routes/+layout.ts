7ced42ddcf3750a41b1b6d494c71e91b0fb7411d
Yakiyo <yakiyo1206@gmail.com>
2023-11-13 05:33:54 +0000

    fix: return early on server side load

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,7 @@ import type { Load } from '@sveltejs/kit';
 export const ssr = false;
 
 export const load: Load = async () => {
+	if (import.meta.env.SSR) return;
 	const token = localStorage.getItem('token');
 	if (!token)
 		return {
