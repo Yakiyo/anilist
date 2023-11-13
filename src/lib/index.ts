2396f4d6a3a2e9ef078b03afd6b8b69713e148dd
Yakiyo <yakiyo1206@gmail.com>
2023-11-13 10:20:21 +0000

    feat: dynamic page title generation

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,12 @@
-// place files you want to import through the `$lib` alias in this folder.
+/**
+ * Generate current page's title based on pathname
+ * @param pathname current route's path
+ * @returns string
+ */
+export function pageTitle(pathname: string): string {
+	let path = pathname;
+	if (path === '/') return 'Anilist';
+	path = path.substring(1);
+	path = path.charAt(0).toUpperCase() + path.substring(1);
+	return `${path} · Anilist`;
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,19 @@
 <script>
 	import Header from '../components/layout/Header.svelte';
 	import Footer from '../components/layout/Footer.svelte';
-
+	import { page } from '$app/stores';
 	import '../app.css';
-	import { fetchUser } from '$lib/store';
-	/**
-	 * @type import('./$types').PageData
-	 */
+	import { pageTitle } from '$lib';
+
+	$: title = pageTitle($page.url.pathname);
 	export let data;
 	let username = data.user?.name;
 	let isAuthed = data.isAuthed;
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
 
 <div class="flex flex-col min-h-screen">
 	<Header isAuthed={$isAuthed} {username} />
