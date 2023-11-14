ce307bd071243475fa065524cc426095e4c1b0c8
Yakiyo <yakiyo1206@gmail.com>
2023-11-14 15:05:53 +0000

    feat: more work on footer

--- src/components/layout/Footer.svelte
+++ src/components/layout/Footer.svelte
@@ -1,16 +1,70 @@
 <script>
 	// Site themes, not supporting high contrast cz idk how to implement
 	// three different themes using tailwind
-	const themes = ['Default', 'Dark' /**'High Contrast' */, , 'System Theme'];
+	const themes = ['Default', 'Dark', /**'High Contrast' ,*/ 'System Theme'];
+
+	const links = [
+		[
+			{ name: 'Logout', url: '#' },
+			{ name: 'Donate', url: '/donate' },
+			{ name: 'AniList.co', url: 'https://anilist.co' },
+			{ name: 'AniChar', url: 'https://anichart.net/' },
+		],
+		[
+			{ name: 'Apps', url: '/apps' },
+			{ name: 'Site Stats', url: '/site-sites' },
+			{ name: 'Recommendations', url: '/recommendations' },
+			{ name: 'API', url: 'https://github.com/AniList/ApiV2-GraphQL-Docs' },
+		],
+		/**
+		 * TODO: Add icons in front of the links
+		 */
+		[
+			{ name: 'Discord', url: 'https://discord.gg/TF428cr' },
+			{ name: 'Twitter', url: 'https://twitter.com/AniListco' },
+			{ name: 'Facebook', url: 'https://www.facebook.com/AniListco' },
+			{ name: 'Github', url: 'https://github.com/AniList' },
+		],
+		[
+			{ name: 'Add Data', url: 'https://submission-manual.anilist.co/' },
+			{ name: 'Moderators', url: 'https://anilist.co/moderators' },
+			{ name: 'Contact', url: 'mailto:contact@anilist.co' },
+			{ name: 'Site Map.xml', url: 'https://anilist.co/sitemap/index.xml' },
+		],
+	];
 </script>
 
-<footer class="bg-footer flex justify-evenly flex-wrap text-base">
-	<div>
+<footer class="bg-footer flex justify-around text-base flex-col lg:flex-row">
+	<div class="p-12 grow">
 		<span class="text-sky-400">Site Themes</span>
-		<ul>
-			{#each themes as theme}
-				<li class={theme}>A</li>
-			{/each}
-		</ul>
+		<div class="flex flex-row">
+			<div
+				class="h-6 w-6 m-1 border-4 rounded-lg border-solid border-black text-center flex justify-center items-center bg-white"
+			>
+				<p>A</p>
+			</div>
+			<div
+				class="h-6 w-6 m-1 border-4 rounded-lg border-solid border-black text-center flex justify-center items-center bg-header"
+			>
+				<p>A</p>
+			</div>
+			<div
+				class="h-6 w-6 m-1 border-4 rounded-lg border-solid border-black text-center flex justify-center items-center bg-gray-500"
+			>
+				<p>A</p>
+			</div>
+			<!-- TODO: add high contrast theme here someday -->
+		</div>
+	</div>
+	<div class="flex flex-col lg:flex-row grow-[2]">
+		{#each links as link}
+			<div class="p-12">
+				<ul>
+					{#each link as item}
+						<li><a href={item.url}>{item.name}</a></li>
+					{/each}
+				</ul>
+			</div>
+		{/each}
 	</div>
 </footer>
