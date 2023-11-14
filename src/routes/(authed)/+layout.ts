be02f6748e742f81a1bc53ecd4d88c6c210e08cc
Yakiyo <yakiyo1206@gmail.com>
2023-11-14 11:06:29 +0000

    fix: redirect when not authed

--- src/routes/(authed)/+layout.ts
+++ src/routes/(authed)/+layout.ts
@@ -1,11 +1,12 @@
 import type { BaseLoadValue } from '$lib/models';
 import { redirect } from '@sveltejs/kit';
 import type { LayoutLoad } from '../$types';
+import { get } from 'svelte/store';
 
 // If user not authenticated, redirect them to root
 export const load: LayoutLoad = async ({ parent }) => {
 	const data = (await parent()) as BaseLoadValue;
-	if (!data.isAuthed) {
+	if (!get(data.isAuthed)) {
 		throw redirect(303, '/');
 	}
 	return data;
