05e2ad23180db6d3c23e7a12189e489ce3de49c3
Yakiyo <yakiyo1206@gmail.com>
2023-11-15 04:56:44 +0000

    feat: tiny work

--- src/components/layout/Footer.svelte
+++ src/components/layout/Footer.svelte
@@ -36,12 +36,12 @@
 
 <footer class="bg-footer font-semibold mt-5 py-12 px-8">
 	<!-- the container -->
-	<div class="flex">
+	<div class="flex text-left">
 		<!-- Theme selector -->
 		<div>
 			<h2 class="font-bold text-sky-400 pb-4 text-lg">Site Theme</h2>
 			<div
-				class="cursor-pointer inline-block font-medium h-6 w-6 rounded-sm"
+				class="cursor-pointer inline-block font-medium h-6 w-6 rounded-sm mr-3 pl-[2px] pt-1"
 				style="border: 2px solid #46546b; color: #2e3c48; background: #edf1f5;"
 			>
 				A
