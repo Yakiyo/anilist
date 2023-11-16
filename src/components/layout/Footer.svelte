333fe6ac1fd37fde1949dc94e281c4d80303a1d1
Yakiyo <yakiyo1206@gmail.com>
2023-11-16 17:55:30 +0000

    chore: finish theme selector design

--- src/components/layout/Footer.svelte
+++ src/components/layout/Footer.svelte
@@ -36,13 +36,25 @@
 
 <footer class="bg-footer font-semibold mt-5 py-12 px-8">
 	<!-- the container -->
-	<div class="flex text-left">
+	<div class="flex text-left w-full">
 		<!-- Theme selector -->
 		<div>
 			<h2 class="font-bold text-sky-400 pb-4 text-lg">Site Theme</h2>
 			<div
 				class="cursor-pointer inline-block font-medium h-6 w-6 rounded-sm mr-3 pl-[2px] pt-1"
 				style="border: 2px solid #46546b; color: #2e3c48; background: #edf1f5;"
+			>
+				A
+			</div>
+			<div
+				class="cursor-pointer inline-block font-medium h-6 w-6 rounded-sm mr-3 pl-[2px] pt-1 bg-footer"
+				style="border: 2px solid #46546b;"
+			>
+				A
+			</div>
+			<div
+				class="cursor-pointer inline-block font-medium h-6 w-6 rounded-sm mr-3 pl-[2px] pt-1"
+				style="border: 2px solid #46546b; background: linear-gradient(45deg,#11161d 55%,#edf1f5 0);"
 			>
 				A
 			</div>
