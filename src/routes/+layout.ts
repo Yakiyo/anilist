8fdf81ff69ec6f7bad2f4c00660117381acee18d
Yakiyo <yakiyo1206@gmail.com>
2023-11-17 13:32:03 +0000

    feat: remove unnecessary gymnastics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,18 +1,10 @@
 import type { Load } from '@sveltejs/kit';
 import { token, isAuthed, fetchUser } from '$lib/store/';
-import { writable, readable } from 'svelte/store';
 import type { BaseLoadValue } from '$lib/models';
 
 export const prerender = true;
-export const ssr = false;
 
 export const load: Load = async (): Promise<BaseLoadValue> => {
-	if (import.meta.env.SSR)
-		return {
-			token: writable(null),
-			isAuthed: readable(false),
-			user: null,
-		};
 	return {
 		token,
 		isAuthed,
