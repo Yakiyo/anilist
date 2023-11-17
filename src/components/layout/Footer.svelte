fa2c403002ca09b0eaf9d113433ebbdab53c1cf4
Yakiyo <yakiyo1206@gmail.com>
2023-11-17 13:17:03 +0000

    feat: finish footer

--- src/components/layout/Footer.svelte
+++ src/components/layout/Footer.svelte
@@ -1,24 +1,31 @@
 <script lang="ts">
-	import { faDiscord, faTwitter, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+	import {
+		faDiscord,
+		faTwitter,
+		faFacebook,
+		faGithub,
+		type IconDefinition,
+	} from '@fortawesome/free-brands-svg-icons';
 	import Icon from 'svelte-fa';
 	import { setTheme } from '$lib';
 
-	const links = [
+	const linksArray: {
+		name: string;
+		url: string;
+		icon?: IconDefinition;
+	}[][] = [
 		[
 			{ name: 'Logout', url: '#' },
-			{ name: 'Donate', url: '/donate' },
+			{ name: 'Donate', url: 'https://anilist.co/donate' },
 			{ name: 'AniList.co', url: 'https://anilist.co' },
-			{ name: 'AniChar', url: 'https://anichart.net/' },
+			{ name: 'AniChart', url: 'https://anichart.net/' },
 		],
 		[
-			{ name: 'Apps', url: '/apps' },
-			{ name: 'Site Stats', url: '/site-sites' },
-			{ name: 'Recommendations', url: '/recommendations' },
+			{ name: 'Apps', url: 'https://anilist.co/apps' },
+			{ name: 'Site Stats', url: 'https://anilist.co/site-sites' },
+			{ name: 'Recommendations', url: 'https://anilist.co/recommendations' },
 			{ name: 'API', url: 'https://github.com/AniList/ApiV2-GraphQL-Docs' },
 		],
-		/**
-		 * TODO: Add icons in front of the links
-		 */
 		[
 			{ name: 'Discord', url: 'https://discord.gg/TF428cr', icon: faDiscord },
 			{ name: 'Twitter', url: 'https://twitter.com/AniListco', icon: faTwitter },
@@ -34,11 +41,11 @@
 	];
 </script>
 
-<footer class="bg-footer font-semibold mt-5 py-12 px-8 flex justify-center">
+<footer class="bg-footer text-base font-semibold mt-5 py-12 px-8 flex justify-center">
 	<!-- the container -->
-	<div class="flex text-left w-full xl:max-w-[1140px]">
+	<div class="flex text-left w-full xl:max-w-[1140px] justify-between flex-col lg:flex-row">
 		<!-- Theme selector -->
-		<div>
+		<div class="basis-1/4 my-8">
 			<h2 class="font-bold text-sky-400 pb-4 text-lg">Site Theme</h2>
 			<!-- Default -->
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -75,6 +82,27 @@
 			</div>
 		</div>
 		<!-- Links -->
-		<div />
+		<div class="flex flex-col lg:flex-row justify-around basis-1 grow my-8">
+			{#each linksArray as links, i}
+				<div class="{i === 2 ? 'grow' : ''} my-8">
+					{#each links as link}
+						<div class="mx-4 px-8">
+							<a href={link.url}
+								>{link.name}
+								{#if link.icon}
+									<Icon
+										primaryColor="gray"
+										class="inline"
+										size="sm"
+										style="height: 14px; width: 14px; display: inline;"
+										icon={link.icon}
+									/>
+								{/if}</a
+							>
+						</div>
+					{/each}
+				</div>
+			{/each}
+		</div>
 	</div>
 </footer>
