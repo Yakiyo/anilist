eed5a5feb34db98167ded837957111d6cb79c05d
Yakiyo <yakiyo1206@gmail.com>
2023-11-20 14:48:41 +0000

    feat: use root page name similar to AL

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,7 +8,7 @@ import { storage } from './storage';
  */
 export function pageTitle(pathname: string): string {
 	let path = pathname;
-	if (path === '/') return 'Anilist';
+	if (path === '/') return 'Anilist: Track, Discover, Share Anime and Manga';
 	path = path.substring(1);
 	path = path.charAt(0).toUpperCase() + path.substring(1);
 	return `${path} · Anilist`;
