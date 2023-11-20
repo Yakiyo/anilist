02a7c994a49dd6bccf05145af1a881be19fb52de
Yakiyo <yakiyo1206@gmail.com>
2023-11-20 15:01:56 +0000

    feat: login button

--- src/components/layout/Header.svelte
+++ src/components/layout/Header.svelte
@@ -26,6 +26,7 @@
 			transition:slide={{ delay: 250, duration: 1000, easing: quintOut, axis: 'y' }}
 			class="fixed left-0 right-0 top-0 flex h-16 flex-row items-center justify-around bg-header"
 		>
+			<!-- Logo section -->
 			<div class="flex basis-1/3 justify-center">
 				<a href={isAuthed ? '/home' : '/'}
 					><img
@@ -35,6 +36,7 @@
 					/></a
 				>
 			</div>
+			<!-- Links section -->
 			<nav class="flex basis-1/4 flex-row justify-around">
 				<a href="/home" class="p1 capitalize">home</a>
 				{#if isAuthed && username}
@@ -46,29 +48,35 @@
 				<a href="/search/anime" class="p1 browse capitalize">Browse</a>
 				<a href="/forum/overview" class="p1 capitalize">Forum</a>
 			</nav>
+			<!-- If user authed, show search icon and user avatar, else show login button -->
 			<div class="flex basis-1/3 flex-row justify-center">
-				<svg
-					height="32px"
-					version="1.1"
-					viewBox="0 0 32 32"
-					width="32px"
-					xmlns="http://www.w3.org/2000/svg"
-					xmlns:xlink="http://www.w3.org/1999/xlink"
-					><title /><desc /><defs /><g
-						fill="none"
-						fill-rule="evenodd"
-						id="Page-1"
-						stroke="none"
-						stroke-width="1"
-						><g fill="#929292" id="icon-111-search"
-							><path
-								d="M19.4271164,21.4271164 C18.0372495,22.4174803 16.3366522,23 14.5,23 C9.80557939,23 6,19.1944206 6,14.5 C6,9.80557939 9.80557939,6 14.5,6 C19.1944206,6 23,9.80557939 23,14.5 C23,16.3366522 22.4174803,18.0372495 21.4271164,19.4271164 L27.0119176,25.0119176 C27.5621186,25.5621186 27.5575313,26.4424687 27.0117185,26.9882815 L26.9882815,27.0117185 C26.4438648,27.5561352 25.5576204,27.5576204 25.0119176,27.0119176 L19.4271164,21.4271164 L19.4271164,21.4271164 Z M14.5,21 C18.0898511,21 21,18.0898511 21,14.5 C21,10.9101489 18.0898511,8 14.5,8 C10.9101489,8 8,10.9101489 8,14.5 C8,18.0898511 10.9101489,21 14.5,21 L14.5,21 Z"
-								id="search"
-							/></g
-						></g
-					></svg
-				>
-			</div>
+			{#if isAuthed}
+			
+					<svg
+						height="32px"
+						version="1.1"
+						viewBox="0 0 32 32"
+						width="32px"
+						xmlns="http://www.w3.org/2000/svg"
+						xmlns:xlink="http://www.w3.org/1999/xlink"
+						><title /><desc /><defs /><g
+							fill="none"
+							fill-rule="evenodd"
+							id="Page-1"
+							stroke="none"
+							stroke-width="1"
+							><g fill="#929292" id="icon-111-search"
+								><path
+									d="M19.4271164,21.4271164 C18.0372495,22.4174803 16.3366522,23 14.5,23 C9.80557939,23 6,19.1944206 6,14.5 C6,9.80557939 9.80557939,6 14.5,6 C19.1944206,6 23,9.80557939 23,14.5 C23,16.3366522 22.4174803,18.0372495 21.4271164,19.4271164 L27.0119176,25.0119176 C27.5621186,25.5621186 27.5575313,26.4424687 27.0117185,26.9882815 L26.9882815,27.0117185 C26.4438648,27.5561352 25.5576204,27.5576204 25.0119176,27.0119176 L19.4271164,21.4271164 L19.4271164,21.4271164 Z M14.5,21 C18.0898511,21 21,18.0898511 21,14.5 C21,10.9101489 18.0898511,8 14.5,8 C10.9101489,8 8,10.9101489 8,14.5 C8,18.0898511 10.9101489,21 14.5,21 L14.5,21 Z"
+									id="search"
+								/></g
+							></g
+						></svg
+					>
+			{:else}
+				<button class="bg-sky-400 text-white capitalize rounded hover:shadow-2xl p-2">Login</button>
+			{/if}
+		</div>
 		</header>
 	{/if}
 </div>
