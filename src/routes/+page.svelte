d6caf73e56817a849eba61e5ca413ff8b60dfd5e
Yakiyo <yakiyo1206@gmail.com>
2023-11-22 09:56:53 +0000

    chore: start features section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
-	// const features: { title: string; desc: string; icon: string }[] = [];
+	const features: { title: string; desc: string; icon: string }[] = [
+		{
+			title: 'Discover your obsessions',
+			desc: 'What are your highest rated genres or most watched voice actors? Follow your watching habits over time with in-depth statistics.',
+			icon: '/icons/landing/stats.svg',
+		},
+		{
+			title: 'Bring AniList anywhere',
+			desc: 'Keep track of your progress on-the-go with one of many AniList apps across iOS, Android, macOS, and Windows.',
+			icon: '/icons/landing/apps.svg',
+		},
+		{
+			title: 'Join the conversation',
+			desc: 'Share your thoughts with our thriving community, make friends, socialize, and receive recommendations.',
+			icon: '/icons/landing/socials.svg',
+		},
+		{
+			title: 'Tweak it to your liking',
+			desc: 'Customize your scoring system, title format, color scheme, and much more! Also, we have a dark mode.',
+			icon: '/icons/landing/custom.svg',
+		},
+	];
 </script>
 
 <div class="flex w-full flex-col items-center justify-center">
@@ -12,8 +33,16 @@
 			Track, share and discover your favourite anime and manga with Anilist
 		</h2>
 		<!-- Features section -->
-		<div style="border: solid 2px red;">
-			<img src="/icons/landing/stats.svg" class="h-w-20 w-20" alt="stats" />
+		<div class="grid grid-cols-4 gap-16">
+			{#each features as feature}
+				<div>
+					<img src={feature.icon} alt="feature-icon" />
+					<div>
+						<h3>{feature.title}</h3>
+						<div>{feature.desc}</div>
+					</div>
+				</div>
+			{/each}
 		</div>
 	</div>
 	<div>Search</div>
