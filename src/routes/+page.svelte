5ce5827234653afb922c16133971c98fdb16926e
Yakiyo <yakiyo1206@gmail.com>
2023-11-22 04:26:16 +0000

    feat: some work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-<h1>In Main</h1>
+<script lang="ts">
+	// const features: { title: string; desc: string; icon: string }[] = [];
+</script>
+
+<div class="flex w-full flex-col items-center justify-center">
+	<!-- Landing section -->
+	<div
+		class="mx-0 my-auto mb-5 flex max-w-[1100px] flex-col items-center justify-center rounded-[20px] bg-[#0a1625] px-20 py-[60px]"
+	>
+		<h1 class="p-3 text-3xl font-bold text-[#f0f3f6]">The next-generation anime platform</h1>
+		<h2 class="mx-0 my-auto max-w-[370px] p-3 text-center text-lg text-[#acd5f2]">
+			Track, share and discover your favourite anime and manga with Anilist
+		</h2>
+		<!-- Features section -->
+		<div style="border: solid 2px red;">
+			<img src="/icons/landing/stats.svg" class="h-w-20 w-20" alt="stats" />
+		</div>
+	</div>
+	<div>Search</div>
+</div>
